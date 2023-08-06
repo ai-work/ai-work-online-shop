5c4e85667356d5ffdf9bc951547f9bce406c3232
ai-work <ai-work>
2023-08-06 07:57:55 +0200

    DB schema additions

--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,6 @@
 import { InferModel, relations, sql } from "drizzle-orm";
 import { boolean, integer, numeric, pgEnum, pgTable, serial, text, timestamp, uniqueIndex, varchar } from "drizzle-orm/pg-core";
+import { unique } from "next/dist/build/utils";
 
 export const users = pgTable('users', { 
     id: serial('id').primaryKey(),
@@ -96,19 +97,19 @@ export const products = pgTable('products', {
     isFeatured: boolean('is_featured'),
     isArchived: boolean('is_archived').default(false),
     sizeId: integer('size_id').references(() => sizes.id), // Foreign Key to Size
-    size        Size      @relation(fields: [sizeId], references: [id])
+    // size        Size      @relation(fields: [sizeId], references: [id])
     colorId: integer('color_id').references(() => colors.id), // Foreign Key to Color
-    color       Color     @relation(fields: [colorId], references: [id])
-    images      Image[]   // Relation to Image model
-    orderItems  OrderItem[]   // Relation to Order model
+    // color       Color     @relation(fields: [colorId], references: [id])
+    // images      Image[]   // Relation to Image model
+    // orderItems  OrderItem[]   // Relation to Order model
     createdAt: timestamp('created_at').defaultNow(),
     updatedAt: timestamp('updated_at')
 }, (products) => {
     return {
         storeIndex: uniqueIndex('store_idx').on(products.storeId),
         categoryIndex: uniqueIndex('category_idx').on(products.categoryId),
-        storeIndex: uniqueIndex('size_idx').on(products.sizeId),
-        storeIndex: uniqueIndex('color_idx').on(products.colorId),
+        sizeIndex: uniqueIndex('size_idx').on(products.sizeId),
+        colorIndex: uniqueIndex('color_idx').on(products.colorId),
     }
 });
 
@@ -130,13 +131,16 @@ export const productsToCategoryRelations = relations(products, ({ one }) => ({
 export const orders = pgTable('orders', {
     id: serial('id').primaryKey(),
     storeId: integer('store_id').references(() => stores.id), // Foreign Key to Store
-    orderItems OrderItem[] // Relation to OrderItem model
+    // orderItems OrderItem[] // Relation to OrderItem model
     isPaid: boolean('is_paid').default(false),
     phone: varchar('phone', { length: 32 }),
-    address: varchar('address', { length: 64 })    String    @default("")
+    address: varchar('address', { length: 64 }),
     createdAt: timestamp('created_at').defaultNow(),
     updatedAt: timestamp('updated_at'),
-    @@index([storeId])
+}, (orders) => {
+    return {
+        storeIndex: uniqueIndex('store_idx').on(orders.storeId)
+    }
 });
 
 export const storeToOrdersRelations = relations(stores, ({ many }) => ({
@@ -150,30 +154,32 @@ export const ordersToStoreRelations = relations(orders, ({ one }) => ({
 	})
 }));
 
-  // Intermediary for a many-to-many relationship
-export const orderItems = pgTable('order_items', {
-    id: serial('id').primaryKey(),
-    orderId   String  // Foreign Key to Order
-    order     Order   @relation(fields: [orderId], references: [id])
-    productId String  // Foreign Key to Product
-    product   Product @relation(fields: [productId], references: [id])
+// Intermediary for a many-to-many relationship
+// export const orderItems = pgTable('order_items', {
+//     id: serial('id').primaryKey(),
+//     orderId   String  // Foreign Key to Order
+//     order     Order   @relation(fields: [orderId], references: [id])
+//     productId String  // Foreign Key to Product
+//     product   Product @relation(fields: [productId], references: [id])
 
-    @@index([orderId])
-    @@index([productId])
-});
+//     @@index([orderId])
+//     @@index([productId])
+// });
 
 export const sizes = pgTable('sizes', {
     id: serial('id').primaryKey(),
     storeId: integer('store_id').references(() => stores.id), // Foreign Key to Store
-    store       Store     @relation("StoreToSize", fields: [storeId], references: [id])
+    // store       Store     @relation("StoreToSize", fields: [storeId], references: [id])
     name: varchar('name', { length: 256 } ),
     value: varchar('value', { length: 256 } ),
-    products    Product[] // Relation to Product model
+    // products    Product[] // Relation to Product model
     createdAt: timestamp('created_at').defaultNow(),
     updatedAt: timestamp('updated_at'),
-
-    @@index([storeId])
-}
+}, (sizes) => {
+    return {
+        storeIndex: uniqueIndex('store_idx').on(sizes.storeId)
+    }
+});
 
 export const storeToSizesRelations = relations(stores, ({ many }) => ({
 	storeSizes: many(sizes),
@@ -182,15 +188,17 @@ export const storeToSizesRelations = relations(stores, ({ many }) => ({
 export const colors = pgTable('colors', {
     id: serial('id').primaryKey(),
     storeId: integer('store_id').references(() => stores.id), // Foreign Key to Store
-    store       Store    @relation("StoreToColor", fields: [storeId], references: [id])
+    // store       Store    @relation("StoreToColor", fields: [storeId], references: [id])
     name: varchar('name', { length: 256 } ),
     value: varchar('value', { length: 256 } ),
-    products    Product[] // Relation to Product model
+    // products    Product[] // Relation to Product model
     createdAt: timestamp('created_at').defaultNow(),
     updatedAt: timestamp('updated_at'),
-
-    @@index([storeId])
-}
+}, (colors) => {
+    return {
+        storeIndex: uniqueIndex('store_idx').on(colors.storeId)
+    }
+});
 
 export const storeToColorsRelations = relations(stores, ({ many }) => ({
 	storeColors: many(colors),
@@ -199,8 +207,8 @@ export const storeToColorsRelations = relations(stores, ({ many }) => ({
 export const image = pgTable('image', {
     id: serial('id').primaryKey(),
     productId: integer('product_id').references(() => products.id), // Foreign Key to Product
-    product     Product  @relation(fields: [productId], references: [id], onDelete: Cascade)
+    // product     Product  @relation(fields: [productId], references: [id], onDelete: Cascade)
     url: varchar('url', { length: 256 } ),
     createdAt: timestamp('created_at').defaultNow(),
     updatedAt: timestamp('updated_at'),
-}
+});
